9ba2598615628d8cd542a4b5484e34b46e13e389
행복한엄쌤 <sendmethere@naver.com>
2026-03-02 14:26:49 +0900

    fix: replace local fonts with CDN, remove binary files from tracking

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ export default {
       },
       fontFamily: {
         pretendard: [
+          'Pretendard Variable',
           'Pretendard',
           '-apple-system',
           'BlinkMacSystemFont',
